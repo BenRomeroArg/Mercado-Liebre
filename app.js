f944bbec893d72b84d53633c05a7db48a12a2f82
Ben <rbenorlando@gmail.com>
2022-12-06 09:50:33 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, "public")))
+
+app.listen(8000, ()=>{
+    console.log('En linea');
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/home.html"))
+});
+app.get('/error', (req,res)=>{
+    res.send('<h1>Error404.</h1><p>Lo sentimos el sitio no esta disponible.</p><p>Contacto en el home page del sitio web.</p><p>Volver a la pagina principal.</p>')
+})
+//path.resolve(__dirname,"views/index.html")
