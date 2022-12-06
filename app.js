28eb5286fbb984dd287e8e3581d7d110a70b680c
Ben <rbenorlando@gmail.com>
2022-12-06 09:51:17 -0300

    iniciandoRepo

--- app.js
+++ app.js
@@ -13,4 +13,3 @@ app.get('/',(req,res)=>{
 app.get('/error', (req,res)=>{
     res.send('<h1>Error404.</h1><p>Lo sentimos el sitio no esta disponible.</p><p>Contacto en el home page del sitio web.</p><p>Volver a la pagina principal.</p>')
 })
-//path.resolve(__dirname,"views/index.html")
