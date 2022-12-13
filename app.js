6b0a18426be1dd7d8b31406995e900c60569ee57
Ben <rbenorlando@gmail.com>
2022-12-13 11:20:53 -0300

    version 1.3.01

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"views/home.html"))
 });
 app.get('/error', (req,res)=>{
-    res.send('<h1><font size="15">Error404.</font></h1><p>Lo sentimos el sitio no esta disponible.</p><p>Contacto en el home page del sitio web.</p><p>Volver a la pagina principal de <a href="/">Mercado Liebre</a>')
+    res.send('<div align="center"><h1><font size="15">Error404.</font></h1><p>Estamos trabajando para mejorar la experiencia.</p><p>Contacto en el home page del sitio web.</p><p>Volver a la pagina principal de <a href="/">Mercado Liebre</a></div>')
 })
+app.get('/producto-detalle',(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/detalle.html"))
+});
